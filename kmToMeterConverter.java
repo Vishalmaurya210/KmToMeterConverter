bcbc7c22f2cf10a6c095e9152bae49d8279876e7
Vishalmaurya210 <vishaljimaurya2004@gmail.com>
2026-01-21 16:51:06 +0530

    KM to Meter Swing Converter
    Your file kmToMeterConverter.java will be uploaded successfully.

---
+++ kmToMeterConverter.java
@@ -0,0 +1,80 @@
+package SwingBuilder;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class kmToMeterConverter extends JFrame  implements ActionListener {
+    JButton b1, b2 , b3;
+    JLabel l1, l2;
+    JTextField t1, t2;
+    JPanel panel;
+
+    public kmToMeterConverter() {
+        initComponents();
+
+    }
+    public void initComponents() {
+        setTitle("Unit converter");
+
+        panel = new JPanel();
+        b1 = new JButton("Converter");
+        b2 = new JButton("Clear");
+        b3 = new JButton("Quit");
+
+        l1 = new JLabel("Enter km: ");
+        l2 = new JLabel("Meter : ");
+
+        t1 = new JTextField();
+        t2 = new JTextField();
+        t2.setEditable(false);
+
+        l1.setBounds(50, 50, 100, 30);
+        t1.setBounds(50, 50, 150, 30);
+
+        l2.setBounds(50, 50, 100, 30);
+        t2.setBounds(50, 50, 150, 30);
+
+        b1.setBounds(40,140,90,30);
+        b2.setBounds(160,140,90,30);
+        b3.setBounds(260,140,90,30);
+
+        add(l1); add(l2);
+        add(b1); add(b2);
+        add(b3);
+        add(t1); add(t2);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400,400);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae){
+    if(ae.getSource()==b1){
+        double km = Double.parseDouble(t1.getText());
+        double meter = km*1000;
+        t2.setText(String.valueOf(meter));
+    }
+    if(ae.getSource()==b2){
+        t1.setText(" ");
+        t2.setText(" ");
+        t1.requestFocus();
+    }
+    if(ae.getSource()==b3){
+        System.exit(0);
+    }
+
+    }
+
+    public static void main(String[] args) {
+        new kmToMeterConverter();
+    }
+
+
+
+}
